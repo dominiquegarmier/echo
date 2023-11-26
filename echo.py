029d7ae9ecbb7b5ce853100e1a9921711647aa62
Dominique Garmier <dominique@garmier.ch>
2023-11-26 14:36:28 +0100

    small changes

--- echo.py
+++ echo.py
@@ -4,6 +4,7 @@ from collections.abc import Generator
 from collections.abc import MutableSequence
 from contextlib import redirect_stdout
 from io import StringIO
+from textwrap import dedent
 from typing import NamedTuple
 
 from litellm import completion
@@ -13,42 +14,81 @@ from rich.console import Console
 console = Console()
 
 
-MODEL = "ollama/mistral"
+MODEL = "ollama/codellama"
 API_BASE = "http://localhost:11434"
 
 PROMPT = """\
-Your Name is Echo. You are a helpful assistant that knows how to
-programm in python. In order to give better answers you are equipped
-with the ability to generate and execute python code. The code that
-is executed is contained in markdown code blocks of the form
+Your Name is Echo.
+
+To better answer questions you are equipped with the ability to generate
+and execute python code. The user will decide whether to execute the code.
+
+The code has to be given in markdown code blocks of the form:
 
 ```python
 # code goes here
 ```
 
-Dont add python code blocks to your response unless you want to execute them.
-If it makes sense you should use code to find the answer to the question.
-Make Your answer as brief as possible.
-If not absolutely necessary say nothing else than the code.
+Only write python code if you want to execute it.
+Make your answer as brief as possible.
+Unless absolutely necessary say nothing else than the code.
 """
 
-RESPONSE_PROMPT = """\
-the stdout of the code above was:
-
-```
-****
-```
-
-I should quickly explain the answer I got...
-"""
-
-PYTHON_CODE_BLOCK = "```python\n"
-CODE_BLOCK = "```\n"
+PYTHON_CODE_BLOCK = "```python"
+CODE_BLOCK = "```"
 
 
 class Message(NamedTuple):
     content: str
     role: str = "user"
+
+
+def styled(contents: str, style: str) -> None:
+    console.print(contents, style=style, end="")
+    return None
+
+
+def format_exec_response(result) -> Message:
+    ret = f"""\
+    the code returned:
+    {CODE_BLOCK}
+    {result}
+    {CODE_BLOCK}
+    what does this mean with respect to the question?
+    """
+    return Message(content=dedent(ret), role="user")
+
+
+def parse_code(message: Message) -> str | None:
+    content = message.content + "\n"
+    try:
+        content = content.split(PYTHON_CODE_BLOCK + "\n")[1]
+        return content.split(CODE_BLOCK + "\n")[0]
+    except IndexError:
+        return None
+
+
+def exec_code(code: str) -> str:
+    stdout = StringIO()
+    with redirect_stdout(stdout):
+        try:
+            exec(code)
+        except Exception as e:
+            print(e)
+    return stdout.getvalue()
+
+
+def handle_code(code: str) -> tuple[Message, bool]:
+    styled("\nPYTHON CODE DETECTED\n", style="bold red")
+    styled(f"{PYTHON_CODE_BLOCK}\n{code}{CODE_BLOCK}\n\n", style="red")
+    styled("EXECUTE CODE? (y/n): ", style="bold red")
+
+    if input() in ["y", "Y"]:
+        styled("EXECUTING CODE...\n", style="bold red")
+        return format_exec_response(exec_code(code)), False
+    else:
+        err = RuntimeError("User chose not to execute code.")
+        return format_exec_response(err), True
 
 
 def generate(chat: list[Message]) -> Generator[str, None, None]:
@@ -71,80 +111,34 @@ def initialize_chat() -> list[Message]:
     return [prompt]
 
 
-def _parse_code(message: Message) -> str | None:
-    content = message.content + "\n"
-    try:
-        content = content.split(PYTHON_CODE_BLOCK)[1]
-        return content.split(CODE_BLOCK)[0]
-    except IndexError:
-        return None
+def reply(chat: MutableSequence[Message]) -> None:
+    styled("echo: ", style="bold green")
 
+    agg = ""
+    for text in generate(list(chat)):
+        styled(text, style="blue")
+        agg += text
 
-def _exec_code(code: str) -> str:
-    stdout = StringIO()
-    with redirect_stdout(stdout):
-        try:
-            exec(code)
-        except Exception as e:
-            print(e)
-    return stdout.getvalue()
+    styled("\n", style="blue")
+    message = Message(content=agg, role="assistant")
+    chat.append(message)
 
-
-def handle_code(code: str) -> Message | None:
-    styled("\nPYTHON CODE DETECTED\n", style="bold red")
-    styled(f"{PYTHON_CODE_BLOCK}{code}{CODE_BLOCK}\n", style="red")
-    styled("EXECUTE CODE? (y/n): ", style="bold red")
-
-    if input() in ["y", "Y"]:
-        styled("EXECUTING CODE...\n", style="bold red")
-        result = _exec_code(code)
-        wrapped = RESPONSE_PROMPT.replace("****", result)
-        styled(wrapped, style="red")
-        return Message(content=wrapped, role="assistant")
-    else:
-        return None
-
-
-def reply_loop(chat: MutableSequence[Message]) -> None:
-    while True:
-        styled("Echo: ", style="bold blue")
-        response = ""
-        for text in generate(list(chat)):
-            styled(text, style="blue")
-            response += text
-        styled("\n", style="blue")
-
-        message = Message(content=response, role="assistant")
+    code = parse_code(message)
+    if code is not None:
+        message, _ = handle_code(code)
+        styled(message.content, style="bold red")
         chat.append(message)
-
-        code = _parse_code(message)
-        if code is not None:
-            message = handle_code(code)
-            if message is None:
-                break
-            chat.append(message)
-        else:
-            break
-
-
-def styled(contents: str, style: str) -> None:
-    console.print(contents, style=style, end="")
-    return None
 
 
 def main() -> int:
     styled("Welcome to Echo!\n\n", style="bold blue")
-
     chat = initialize_chat()
     try:
         while True:
             styled("> ", style="bold green")
-            promp = input()
-
-            message = Message(content=promp, role="user")
+            message = Message(content=input(), role="user")
             chat.append(message)
-            reply_loop(chat)
-
+            reply(chat)
     except KeyboardInterrupt:
         styled("\n\nGoodbye!\n", style="bold blue")
         return 0
